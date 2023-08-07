76e30467f4ff6981ffb2bf0f8dda7eb8babffc77
omarzaqout <omar02-2012@hotmail.com>
2023-08-07 08:49:49 -0700

    test conn

--- src/main/java/conn/connect.java
+++ src/main/java/conn/connect.java
@@ -2,40 +2,44 @@ package conn;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Logger;
 import java.sql.Statement;
+import java.util.logging.Logger;
+
 
 public class connect {
 	protected Connection connection;
-	private static final Logger logger = Logger.getLogger(connect.class.getName());
-	private String pass="123456";
+	 private static final Logger logger = Logger.getLogger(connect.class.getName());
     public void func() throws SQLException {
 
-    	try {
-    	    Class.forName("com.mysql.cj.jdbc.Driver");
-    	    String url = "jdbc:mysql://localhost:3306/sakancom";
-    	    String user = "root";
-    	    String pass = "new_password"; // Replace 'new_password' with the new secure password
-
-    	    try (Connection connection = DriverManager.getConnection(url, user, pass);
-    	         Statement statement = connection.createStatement()) {
-    	        
-    	       
-    	        statement.executeUpdate("ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_password';");
-
-    	       
-
-    	    } catch (SQLException e) {
-    	     
-    	    	 logger.info("An SQL exception occurred: " + e);
-    	    }
-    	} catch (ClassNotFoundException ex) {
-    	   
-    	    logger.info("Failed to load MySQL JDBC driver");
-    	}
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            		connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/sakancom","root","");
+            		
+            		logger.info("success conected");
+        } catch (ClassNotFoundException e) {
+        	logger.info("An SQL exception occurred"+e);
+        }
     }
-    
+
+    public void executeStatement(String sqlStatement) {
+    	 try (Statement statement = connection.createStatement()) {
+
+                ResultSet resultSet = statement.executeQuery(sqlStatement);
+
+                if (resultSet.next()) {
+                    int totalRows = resultSet.getInt("total_rows");
+                    logger.info("Total rows in the users table: " + totalRows);
+                }
+                else {
+                    logger.info("Connection is not established. Please call establishConnection() first.");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
 	public Connection getConnection() {
 		return connection;
