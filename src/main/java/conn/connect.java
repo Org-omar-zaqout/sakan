6d6cae0864c6121936b40c9c0dce8e301ecc86cd
omarzaqout <omar02-2012@hotmail.com>
2023-08-07 09:11:26 -0700

    add password xml pro

--- src/main/java/conn/connect.java
+++ src/main/java/conn/connect.java
@@ -1,27 +1,39 @@
 package conn;
 
 import java.sql.Connection;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Logger;
-
+import java.util.Properties;
 
 public class connect {
 	protected Connection connection;
 	 private static final Logger logger = Logger.getLogger(connect.class.getName());
     public void func() throws SQLException {
+    	 Properties properties = new Properties();
+    	 try (FileInputStream fis = new FileInputStream("D:\\Software\\untitled4\\src\\main\\java\\conn\\db.properties")) {
+             properties.load(fis);
+             String url = properties.getProperty("db.url");
+             String user = properties.getProperty("db.user");
+             String pass = "db.password";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String password="123456";
-            		connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/sakancom","root",password);
+            connection = DriverManager.getConnection(url, user, pass);
             		
             		logger.info("success conected");
         } catch (ClassNotFoundException e) {
         	logger.info("An SQL exception occurred"+e);
         }
+    	 } 
+    	 catch (IOException e1) {
+			
+			logger.info("Error"+e1);
+		}
     }
 
     public void executeStatement(String sqlStatement) {
